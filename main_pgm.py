c901267a9208e81ddf1e7ae47f8cef1d68c0ea29
Rambesz <rambihno88@gmail.com>
2021-02-12 22:38:05 +0100

    debug data removal

--- main_pgm.py
+++ main_pgm.py
@@ -2,11 +2,9 @@ from functions import*
 
 def Main():
 	tabla = BoardDecimal()
-	tablaa = [0,1,2,3,4]
-	print(tabla.getFirstUpperRow())
-	
-	print(tabla.getFirstLeftColumn())
-	#tabla.printDefaultField()
+	#print(tabla.getFirstUpperRow())	
+	#print(tabla.getFirstLeftColumn())
+	tabla.printField()
 
 if __name__ == '__main__':
 	Main()
